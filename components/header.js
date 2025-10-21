092d59240bacffe2fc7df5cac695fb71322e3d6a
Siddharth Kaushik <sidharth.basu@icloud.com>
2025-10-21 08:07:14 -0700

    add photo

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <header>
       <div className="max-w-xl mx-auto">
         <NextLink href="/" className="flex items-center justify-center gap-2">
-          <Avatar src="/images/pedro-3.jpeg" alt="Faisal Pedro" width={24} height={24} />
+          <Avatar src="/images/pedro.jpeg" alt="Faisal Pedro" width={24} height={24} />
           <h1 className="text-l text-gray-500 font-semibold">Faisal Pedro</h1>
         </NextLink>
       </div>
