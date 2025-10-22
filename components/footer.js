128a831c53276a2b431d05e1412c75076bdfffd9
Siddharth Kaushik <sidharth.basu@icloud.com>
2025-10-21 19:31:02 -0700

    use icons

--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,24 @@
-import { PhoneIcon, UserIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
 import Link from "@/components/link";
+import Icon from "@/components/icon";
 
 export default function Footer() {
   return (
     <footer>
-      <div className="max-w-xl mx-auto">
-        <div className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center text-sm">
+      <div className="max-w-md py-9 mx-auto">
+        <div className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
           <Link href="tel:+17788776997" className="group">
-            <PhoneIcon className="size-4" />
+            <Icon name="phone" size={16} />
             Phone
           </Link>
           <Link href="mailto:fslpdr@gmail.com" className="group">
-            <EnvelopeIcon className="size-4" />
+            <Icon name="envelope" size={16} />
             Email
           </Link>
           <Link
             href="https://www.linkedin.com/in/faisal-pedro-1412b1161/"
             className="group"
           >
-            <UserIcon className="size-4" />
+            <Icon name="user" size={16} />
             LinkedIn
           </Link>
         </div>
