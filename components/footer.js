5d00b6461244d406e0ee6694caf61a22ea949a95
Siddharth Kaushik <sidharth.basu@icloud.com>
2025-10-24 01:12:13 -0700

    prettify text

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="px-3">
       <div className="max-w-md py-9 mx-auto">
-        <div className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           <Link href="tel:+17788776997" className="group">
             <Icon name="phone" size={16} />
             Phone
