7a9eb3ed516e44e2066c324b354b5ae8fbe8c112
Siddharth Kaushik <sidharth.basu@icloud.com>
2025-10-24 01:44:09 -0700

    action chips for header

--- components/header.js
+++ components/header.js
@@ -1,8 +1,13 @@
 import NextLink from "next/link";
 import Avatar from "@/components/avatar";
 import Heading from "@/components/heading";
+import Chip from "@/components/chip";
+import Link from "@/components/link";
+import data from "@/app/data.json";
 
 export default function Header() {
+  const { contact_info, contact_links } = data;
+
   return (
     <header className="px-3">
       <div className="max-w-md py-9 mx-auto">
@@ -10,11 +15,29 @@ export default function Header() {
           href="/"
           className="flex flex-col items-center justify-center gap-2"
         >
-          <Avatar src="/images/pedro.jpeg" alt="Faisal Pedro" size={80} />
-          <Heading level={1} className="text-lg text-gray-500 font-medium">
-            Faisal Pedro
+          <Avatar src={contact_info.avatar} alt={contact_info.name} size={80} />
+          <Heading level={1} className="text-lg text-gray-600 font-semibold">
+            {contact_info.name}
           </Heading>
         </NextLink>
+
+        <div className="flex flex-wrap justify-center gap-2 mt-3">
+          {contact_links.map((link) => (
+            <Link
+              key={link.label}
+              href={link.href}
+              className="no-underline"
+              {...(link.download && { download: true })}
+            >
+              <Chip
+                iconName={link.iconName}
+                className="cursor-pointer hover:bg-gray-200 transition-colors"
+              >
+                {link.label}
+              </Chip>
+            </Link>
+          ))}
+        </div>
       </div>
     </header>
   );
