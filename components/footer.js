bcd6938c95833ae0b68342d6cd56a6b184e9908d
Siddharth Kaushik <sidharth.basu@icloud.com>
2025-10-24 01:44:20 -0700

    resume link in footer

--- components/footer.js
+++ components/footer.js
@@ -1,26 +1,25 @@
 import Link from "@/components/link";
 import Icon from "@/components/icon";
+import data from "@/app/data.json";
 
 export default function Footer() {
+  const contactLinks = data.contact_links;
+
   return (
-    <footer className="px-3">
-      <div className="max-w-md py-9 mx-auto">
-        <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <Link href="tel:+17788776997" className="group">
-            <Icon name="phone" size={16} />
-            Phone
-          </Link>
-          <Link href="mailto:fslpdr@gmail.com" className="group">
-            <Icon name="envelope" size={16} />
-            Email
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/faisal-pedro-1412b1161/"
-            className="group"
-          >
-            <Icon name="user" size={16} />
-            LinkedIn
-          </Link>
+    <footer>
+      <div className="max-w-xl py-9 mx-auto">
+        <div className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+          {contactLinks.map((link) => (
+            <Link
+              key={link.label}
+              href={link.href}
+              className="group"
+              {...(link.download && { download: true })}
+            >
+              <Icon name={link.iconName} size={16} />
+              {link.label}
+            </Link>
+          ))}
         </div>
       </div>
     </footer>
