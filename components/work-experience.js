2e140cef0d6fac0e8ffcd07e1de5710be6a1310c
Siddharth Kaushik <sidharth.basu@icloud.com>
2025-11-05 08:23:40 -0800

    add album link

--- components/work-experience.js
+++ components/work-experience.js
@@ -1,16 +1,18 @@
-import Heading from "./heading";
-import List from "./list";
 import data from "@/app/data.json";
+
+import Heading from "./heading";
+import Icon from "./icon";
+import Link from "./link";
+import List from "./list";
 
 export default function WorkExperience() {
   const experiences = data.work_experience;
 
   return (
     <section className="max-w-lg py-12 mx-auto">
-      <Heading level={2} className="mb-8">
+      <Heading level={2} className="mb-6">
         Work Experience
       </Heading>
-
       <div className="space-y-8">
         {experiences.map((exp, index) => (
           <div
@@ -24,8 +26,20 @@ export default function WorkExperience() {
 
             {/* Second column: Details */}
             <div>
-              <Heading level={3}>{exp.company}</Heading>
-              <p className="text-gray-600 text-sm mb-3">{exp.job_title}</p>
+              <div className="flex items-start justify-between gap-4 mb-3">
+                <div>
+                  <Heading level={3}>{exp.company}</Heading>
+                  <p className="text-gray-600 text-sm">{exp.job_title}</p>
+                </div>
+                {exp.photo_album && (
+                  <Link
+                    href={exp.photo_album}
+                    className="text-sm whitespace-nowrap"
+                  >
+                    Photos <Icon name="arrow-up-right" />
+                  </Link>
+                )}
+              </div>
               <List items={exp.achievements} />
             </div>
           </div>
